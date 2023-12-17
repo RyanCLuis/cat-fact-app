181b7a0fd2fcb45d85ce274cfae878fd16d143d6
Ryan Luis <RyanCLuis@gmail.com>
2023-12-17 10:29:39 -0800

    Hits MVP

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const ROOT_URL = 'https://catfact.ninja'
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Cats' });
 });
+
+router.get('/cats/fact', async function(req, res, next) {
+  const catData = await fetch (`${ROOT_URL}/fact`) 
+  .then(res=>res.json())
+  res.render('cat/fact', { title: 'Cat Fact', catData });
+});
+
 
 module.exports = router;
